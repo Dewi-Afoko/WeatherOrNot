9eff1345e6e47a5e4f8175f07c9d6afb64b6e101
Dewi Afoko <dewi.afoko@gmail.com>
2024-10-15 08:09:06 +0100

    setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I KNOW React!
+          I KNOW React, bro!
         </a>
       </header>
     </div>
