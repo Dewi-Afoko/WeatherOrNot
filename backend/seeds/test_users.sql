860715711e5d80c37935a4c2e267e4b40ea89dcd
Dewi Afoko <dewi.afoko@gmail.com>
2024-10-15 10:47:29 +0100

    sql file created

---
+++ backend/seeds/test_users.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username text,
+    password text
+);
+
+INSERT INTO users (username, password) VALUES ('Dewi', 'password');
+INSERT INTO users (username, password) VALUES ('Edgar', 'Password');
