bf8b00d514258c359652290d26ee0509007f5faf
Chris <cajcrook@gmail.com>
2024-10-23 16:01:39 +0100

    user repo tests

--- backend/tests/test_user_repository.py
+++ backend/tests/test_user_repository.py
@@ -15,6 +15,7 @@ def test_find_user_on_empty_db(db_connection):
     user = repository.find_by_username('NonExistingUser')
     assert user is None
 
+
 def test_create_user_on_empty_db(db_connection):
     repository = UserRepository(db_connection)
     new_user = User(None, 'Test', 'password')
@@ -26,9 +27,10 @@ def test_create_user_on_empty_db(db_connection):
     assert bcrypt.checkpw(password_bytes, user.password.encode('utf-8'))
 
 
+
 def test_add_details_after_user_creation_empty_db(db_connection):
     repository = UserRepository(db_connection)
-    repository.delete_user('Username_Test')
+    repository.delete_all_users()
     new_user = User(None, 'Username_Test', 'password')
     repository.create_user(new_user)
     response = repository.add_details('Username_Test', 'FirstName_Test', 'LastName_Test', '1988_04_11', "201cm", "90") 
@@ -48,5 +50,78 @@ def test_find_user_by_username(db_connection):
     repository.create_user(new_user)
     user = repository.find_by_username('Test')
     assert user.username == 'Test'
+
+
+
+def test_get_all_users_details(db_connection):
+    repository = UserRepository(db_connection)
+    repository.delete_all_users()
+    new_user = User(None, 'Username_Test', 'password')
+    repository.create_user(new_user)
+    response = repository.add_details('Username_Test', 'FirstName_Test', 'LastName_Test', '1988_04_11', "201cm", "90") 
+    assert response == "Details Updated"
+    updated_user = repository.find_by_username('Username_Test')
+    assert updated_user.username == 'Username_Test'
+    assert updated_user.first_name == 'FirstName_Test'
+    assert updated_user.last_name == 'LastName_Test'
+    assert updated_user.dob == '1988_04_11'
+    assert updated_user.height == "201cm"
+    assert updated_user.weight == [90]
+    details = repository.user_details('Username_Test')
+    assert details[0] == 'FirstName_Test'
+    assert details[1] == 'LastName_Test'
+    assert details[2] == '1988_04_11'
+    assert details[3] == '201cm'
+
+
+def test_gets_one_favourite_exercise(db_connection):
+    repository = UserRepository(db_connection)
+    repository.delete_all_users()
+    new_user = User(None, 'Username_Test', 'password')
+    repository.create_user(new_user)
+    exercise = 'Exercise_Test'
+    repository.add_exercise('Username_Test',exercise)
+    exercise_list = repository.find_favourite_exercises('Username_Test')
+    assert exercise_list[0] == 'Exercise_Test'
+    
+    
+def test_gets_multiple_favourite_exercises(db_connection):
+    repository = UserRepository(db_connection)
+    repository.delete_all_users()
+    new_user = User(None, 'Username_Test', 'password')
+    repository.create_user(new_user)
+    exercise_1 = 'Exercise_Test_1'
+    repository.add_exercise('Username_Test',exercise_1)
+    exercise_2 = 'Exercise_Test_2'
+    repository.add_exercise('Username_Test',exercise_2)
+    exercise_3 = 'Exercise_Test_3'
+    repository.add_exercise('Username_Test',exercise_3)
+    exercise_list = repository.find_favourite_exercises('Username_Test')
+    assert exercise_list[0] == 'Exercise_Test_1'
+    assert exercise_list[1] == 'Exercise_Test_2'
+    assert exercise_list[2] == 'Exercise_Test_3'
+
+
+def test_delete_exercise_from_list(db_connection):
+    repository = UserRepository(db_connection)
+    repository.delete_all_users()
+    new_user = User(None, 'Username_Test', 'password')
+    repository.create_user(new_user)
+    exercise_1 = 'Exercise_Test_1'
+    repository.add_exercise('Username_Test',exercise_1)
+    exercise_2 = 'Exercise_Test_2'
+    repository.add_exercise('Username_Test',exercise_2)
+    exercise_3 = 'Exercise_Test_3'
+    repository.add_exercise('Username_Test',exercise_3)
+    exercise_list = repository.find_favourite_exercises('Username_Test')
+    assert exercise_list[0] == 'Exercise_Test_1'
+    assert exercise_list[1] == 'Exercise_Test_2'
+    assert exercise_list[2] == 'Exercise_Test_3'
+    repository.delete_exercise('Username_Test', 'Exercise_Test_2')
+    exercise_list = repository.find_favourite_exercises('Username_Test')
+    assert exercise_list[0] == 'Exercise_Test_1'
+    assert exercise_list[1] == 'Exercise_Test_3'
+
+
 
 
