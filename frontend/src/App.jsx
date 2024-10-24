c448657fc682b5461d8d3f0683e184db77f70ea3
Dewi Afoko <dewi.afoko@gmail.com>
2024-10-24 14:47:30 +0100

    functional

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,58 +6,13 @@ import { LoginPage } from "./pages/Login/LoginPage";
 import { SignupPage } from "./pages/Signup/SignupPage";
 import { UserPage } from "./pages/User/UserPage";
 import { GenerateExercises } from "./pages/GenerateExercises/GenerateExercises";
-
 import { Workouts } from "./pages/Workouts/Workouts";
-
+// Component to handle conditional rendering of the Header
 const Layout = ({ children }) => {
   const location = useLocation();
-  
   // Hide the header for the /login, /signup, and / paths
   const hideHeaderPaths = ["/login", "/signup", "/"];
-  
   const shouldHideHeader = hideHeaderPaths.includes(location.pathname);
-
-
-// import { CreateWorkout } from "./components/CreateWorkout";
-import { Workouts } from "./pages/Workouts/Workouts";
-import ExerciseDetails from "./components/ExerciseDetails";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-// docs: https://reactrouter.com/en/main/start/overview
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomePage />,
-  },
-  {
-    path: "/login",
-    element: <LoginPage />,
-  },
-  {
-    path: "/signup",
-    element: <SignupPage />,
-  },
-  {
-    path: "/user",
-    element: <UserPage />,
-  },
-  {
-    path: "/generate_exercises",
-    element: <GenerateExercises />,
-  },
-  {
-    path: "/workouts",
-    element: <Workouts />,
-  },
-  {
-    path: "/exercise",
-    element: <ExerciseDetails />,
-  },
-]);
-
-
-function App() {
-
   return (
     <>
       {!shouldHideHeader && <Header />} {/* Show header unless it's a restricted path */}
@@ -65,7 +20,6 @@ function App() {
     </>
   );
 };
-
 function App() {
   return (
     <BrowserRouter>
@@ -82,5 +36,4 @@ function App() {
     </BrowserRouter>
   );
 }
-
 export default App;
