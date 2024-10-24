5db776a9defffafb6094da5657b66d0177b53ce7
Chris <cajcrook@gmail.com>
2024-10-24 10:30:31 +0100

    test workout py

---
+++ backend/tests/test_workout.py
@@ -0,0 +1,67 @@
+from lib.workout import Workout
+
+def test_construct():
+    workout = Workout("test_workout", "planning_false")
+    assert workout.user_username == "test_workout"
+    assert workout.planning_mode == "planning_false"
+
+######### FOR THE BELOW TESTS TO PASS - ADD_EXERCISE IN WORKOUT.PY SHOULD READ 
+#  if len(self.exercise_list) == 0 or self.exercise_list[-1]['complete'] == True or self.planning_mode == True:
+######### OLD correct code commented out in workout.py
+def test_add_exercise():
+    workout = Workout("test_workout", "planning_false")
+    exercise = type('Exercise', (object,), {'name': 'test_exercise'})()
+    workout.add_exercise(exercise)
+    assert workout.exercise_list[-1] == {
+        'name': "test_exercise",
+        'loading': [],
+        'reps': [],
+        'complete': False
+    }   
+
+
+def test_add_loading():
+    workout = Workout("test_workout", "planning_false")
+    exercise = type('Exercise', (object,), {'name': 'test_exercise'})()
+    workout.add_exercise(exercise) 
+    workout.add_loading(50) 
+    assert workout.exercise_list[-1]['loading'] == ['50kg']
+
+
+def test_add_reps_under_six():
+    workout = Workout("test_workout", "planning_false")
+    exercise = type('Exercise', (object,), {'name': 'test_exercise'})()
+    workout.add_exercise(exercise) 
+    workout.add_reps(10)  
+    assert workout.exercise_list[-1]['reps'] == [10]
+    assert workout.exercise_list[-1]['complete'] == False
+
+
+def test_add_reps_until_complete():
+    workout = Workout("test_workout", "planning_false")
+    exercise = type('Exercise', (object,), {'name': 'test_exercise'})()
+    workout.add_exercise(exercise) 
+    for _ in range(6):
+        workout.add_reps(10)
+    assert workout.exercise_list[-1]['reps'] == [10, 10, 10, 10, 10, 10]
+    assert workout.exercise_list[-1]['complete'] == True
+
+def test_complete_exercise():
+    workout = Workout("test_workout", "planning_false")
+    exercise = type('Exercise', (object,), {'name': 'test_exercise'})()
+    workout.add_exercise(exercise) 
+    workout.complete_exercise()  
+    assert workout.exercise_list[-1]['complete'] == True    
+
+def test_complete_workout_not_in_planning_mode():
+    workout = Workout("test_workout", False)
+    workout.complete = False 
+    workout.complete_workout()
+    assert workout.complete == True
+    
+
+def test_complete_workout_in_planning_mode():
+    workout = Workout("test_workout", True)
+    workout.complete = False 
+    workout.complete_workout() 
+    assert workout.complete == False
