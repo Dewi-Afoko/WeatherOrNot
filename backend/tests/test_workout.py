dd2d54675cd6d68847d21be9b0aac7ae666afb37
Chris <cajcrook@gmail.com>
2024-10-24 10:34:27 +0100

    test workout py

--- backend/tests/test_workout.py
+++ backend/tests/test_workout.py
@@ -1,15 +1,15 @@
 from lib.workout import Workout
 
 def test_construct():
-    workout = Workout("test_workout", "planning_false")
+    workout = Workout("test_workout", False)
     assert workout.user_username == "test_workout"
-    assert workout.planning_mode == "planning_false"
+    assert workout.planning_mode == False
 
 ######### FOR THE BELOW TESTS TO PASS - ADD_EXERCISE IN WORKOUT.PY SHOULD READ 
 #  if len(self.exercise_list) == 0 or self.exercise_list[-1]['complete'] == True or self.planning_mode == True:
 ######### OLD correct code commented out in workout.py
 def test_add_exercise():
-    workout = Workout("test_workout", "planning_false")
+    workout = Workout("test_workout", False)
     exercise = type('Exercise', (object,), {'name': 'test_exercise'})()
     workout.add_exercise(exercise)
     assert workout.exercise_list[-1] == {
@@ -19,9 +19,16 @@ def test_add_exercise():
         'complete': False
     }   
 
+def test_add_exercise_else_condition():
+    workout = Workout("test_workout", False) 
+    exercise1 = type('Exercise', (object,), {'name': 'first_exercise'})()
+    workout.add_exercise(exercise1)
+    result = workout.add_exercise(type('Exercise', (object,), {'name': 'second_exercise'})()) 
+    assert result == "To add a new exercise, mark the previously added exercise to complete"
+    assert len(workout.exercise_list) == 1
 
 def test_add_loading():
-    workout = Workout("test_workout", "planning_false")
+    workout = Workout("test_workout", False)
     exercise = type('Exercise', (object,), {'name': 'test_exercise'})()
     workout.add_exercise(exercise) 
     workout.add_loading(50) 
@@ -29,7 +36,7 @@ def test_add_loading():
 
 
 def test_add_reps_under_six():
-    workout = Workout("test_workout", "planning_false")
+    workout = Workout("test_workout", False)
     exercise = type('Exercise', (object,), {'name': 'test_exercise'})()
     workout.add_exercise(exercise) 
     workout.add_reps(10)  
@@ -38,7 +45,7 @@ def test_add_reps_under_six():
 
 
 def test_add_reps_until_complete():
-    workout = Workout("test_workout", "planning_false")
+    workout = Workout("test_workout", False)
     exercise = type('Exercise', (object,), {'name': 'test_exercise'})()
     workout.add_exercise(exercise) 
     for _ in range(6):
@@ -47,7 +54,7 @@ def test_add_reps_until_complete():
     assert workout.exercise_list[-1]['complete'] == True
 
 def test_complete_exercise():
-    workout = Workout("test_workout", "planning_false")
+    workout = Workout("test_workout", False)
     exercise = type('Exercise', (object,), {'name': 'test_exercise'})()
     workout.add_exercise(exercise) 
     workout.complete_exercise()  
@@ -65,3 +72,4 @@ def test_complete_workout_in_planning_mode():
     workout.complete = False 
     workout.complete_workout() 
     assert workout.complete == False
+
